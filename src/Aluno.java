55b53ccb8b3dadaf2d24456f3999ba8ae93e6dfc
Daniela Leguari <danielaleguari@outlook.com>
2022-08-18 10:23:38 -0300

    ajustes nas classes Main e Aluno

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Aluno {
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private String endereco;
+
+    //construtor
+    public Aluno(String nome, String matricula) {  // nome e matrícula sendo obrigatórios
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String turma) {
+        this.endereco = turma;
+    }
 }
